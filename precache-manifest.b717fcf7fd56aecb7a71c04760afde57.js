10ba0bf727f0da4ed4724ad7ed00a29b36f3aa94
Carlos Garcia <ceagarcia@gmail.com>
2021-10-15 14:47:22 -0300

    deploy

---
+++ precache-manifest.b717fcf7fd56aecb7a71c04760afde57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccf0514818ce597b6ea",
+    "url": "/css/chunk-vendors.3a280c32.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "f1afdc071fe85dda64a087ed50a0304e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a278c44afc37405880",
+    "url": "/js/about.e8561883.js"
+  },
+  {
+    "revision": "2681e7463e3319075745",
+    "url": "/js/app.4778bc10.js"
+  },
+  {
+    "revision": "fccf0514818ce597b6ea",
+    "url": "/js/chunk-vendors.a1a05de6.js"
+  },
+  {
+    "revision": "c6d4192c9513ac988eacf0b96d86cf5c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
